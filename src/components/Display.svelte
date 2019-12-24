2389adaf33587c246d31af1b2f6a09da325803e1
Ashutosh <ashutoshpanda3714@gmail.com>
2019-12-24 15:56:33 +0530

    Display

---
+++ src/components/Display.svelte
@@ -0,0 +1,39 @@
+<style>
+
+</style>
+
+<div class="p-16">
+  <h1 class="text-blue-900 text-center text-3xl pb-8 ">
+    <b>Here’s how Upwork helps deliver the best match</b>
+  </h1>
+  <div class=" flex justify-center justify-between p-3">
+    <div class="justify-center text-center">
+      <img src="img/talent-work.svg" alt="" />
+      <h2>Tap into an amazing talent network</h2>
+      Get unmatched quality from proven independent professionals and
+      specialized agencies.
+    </div>
+    <div class=" justify-center justify-between p-3">
+      <img src="img/get-a-smater.svg" alt="" />
+      <h3>Get a smarter shortlist, instantly</h3>
+      Our instant matching tech generates a shortlist based on the criteria you
+      select.
+    </div>
+    <div class=" justify-center justify-between p-3">
+      <img src="img/manage-everythinng.svg" alt="" />
+      <h4>Manage everything in one place</h4>
+      Manage shortlisting, interviewing, onboarding, and payments all from
+      Upwork.
+    </div>
+    <div class=" justify-center justify-between p-3">
+      <img src="img/get-a-clear.svg" alt="" />
+      <h5>Get a clear view of the work</h5>
+      Easily track project milestones and progress toward the completion of your
+      jobs.
+    </div>
+  </div>
+  <div class=" ">
+    <button class="">Get started</button>
+    <button class="">Get a Demo</button>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,21 @@
 <script>
   import Header from "./../components/Header.svelte";
   import Hero from "./../components/Hero.svelte";
-  import Highlight  from "./../components/Highlight.svelte";
-  import Features  from "./../components/Features.svelte";
-  import Clients  from "./../components/Clients.svelte";
+  import Highlight from "./../components/Highlight.svelte";
+  import Features from "./../components/Features.svelte";
+  import Clients from "./../components/Clients.svelte";
   import Pricing from "./../components/Pricing.svelte";
+  import Display from "./../components/Display.svelte";
   import Dad from "./../components/Dad.svelte";
   import Footer from "./../components/Footer.svelte";
 </script>
- 
 
-
- 
-    <Header/>
-  <Hero/>
-    <Features/>
-    <Highlight/>
-   <Clients/>
-  <Pricing/>
-    <Dad/>
-    <Footer/>
-    
+<Header />
+<Hero />
+<Features />
+<Highlight />
+<Clients />
+<Pricing />
+<Display />
+<Dad />
+<Footer />

e2c31a85295e6eefc473ebe851072da0299c2773
Ashutosh <ashutoshpanda3714@gmail.com>
2019-12-30 14:27:38 +0530

    SmallContent added

---
+++ src/components/features/SmallContent.svelte
@@ -0,0 +1,30 @@
+<style>
+  .c1 {
+    height: 250px;
+    width: 300px;
+  }
+</style>
+
+<div>
+  <h1 class="text-center text-3xl mt-12 mb-2">
+    <b>WHAT’S INCLUDED</b>
+  </h1>
+  <h2 class="text-center text-xl text-gray-900 mb-8">
+    Get 100+ features out of the box with Next Cart's integrated per-transaction
+    pricing
+  </h2>
+  <div class="">
+    <div class="c1 flex border p-4 justify- shadow-2xl m-12 ">
+      <i class="fa fa-clock text-indigo-500 text-6xl mr-4" />
+      <div class="flex flex-col">
+        <b class="mb-2">Fast, predictable payouts to your bank accounts</b>
+        <t1>
+          Control payout timing Unified payouts Like-for-like settlement
+          Transparent payouts
+        </t1>
+        <img src="img/aerrow.png" class="flex ml-24 h-12 w-12" alt="" />
+      </div>
+    </div>
+  </div>
+
+</div>

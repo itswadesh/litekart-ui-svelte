93b5486a8b659e565482c7009fd3803d2ebf3cfb
Swadesh Behera <itswadesh@gmail.com>
2019-12-31 15:12:53 +0530

    -

--- src/components/features/BigContent.svelte
+++ src/components/features/BigContent.svelte
@@ -9,15 +9,11 @@
   <div
     class="order-last lg:order-first leading-loose w-full lg:w-1/2 pr-0 lg:pr-12
     mb-8">
-    <div class="w-full lg:w-1/2" />
     <h1 class="text-4xl mb-2">Payment Gateways</h1>
     <div class="flex">
       <ul class="w-1/2 mx-6">
         <li>Support for multiple payment gateways</li>
-        <li>
-          Multiple payment options- Debit Card, Credit Card, Netbanking, Prepaid
-          Wallets Store Credits
-        </li>
+        <li>Wallets Store Credits</li>
         <li>SSL certified and secure payment gateways</li>
         <li>Location auto­detection for currency</li>
         <li>Offline payments &amp; Cash on Delivery</li>

3e97fc548b40951d6d09746797de3e28f7428986
Swadesh Behera <itswadesh@gmail.com>
2019-12-31 14:37:39 +0530

    features: Small + Big Content

--- src/components/features/BigContent.svelte
+++ src/components/features/BigContent.svelte
@@ -1,24 +1,38 @@
+<style>
+  ul {
+    list-style-type: disc;
+  }
+</style>
+
 <!-- 2nd section -->
 <div class="flex flex-wrap bg-gray-100 px-2 lg:px-64 lg:mb-12">
   <div
     class="order-last lg:order-first leading-loose w-full lg:w-1/2 pr-0 lg:pr-12
     mb-8">
-    <p class="mb-3">
-      <b>
-        Mobile shoppers make up the biggest eCommerce market, and mobile
-        commerce is still growing with more online customers preferring to shop
-        from mobile every year.
-      </b>
-      These days, a mobile-friendly website is essential for your business's
-      growth and success, but it can be hard for a business to stay on top of
-      emerging trends and technology.
-    </p>
-    3dcart is built from the ground up to be mobile-friendly, with no need for
-    you to maintain multiple versions of your website or use custom code to
-    implement important features like Google AMP. We're here to help your
-    business meet the demand with cutting-edge mobile-first design, reach
-    customers everywhere, and give them the shopping experience they're looking
-    for.
+    <div class="w-full lg:w-1/2" />
+    <h1 class="text-4xl mb-2">Payment Gateways</h1>
+    <div class="flex">
+      <ul class="w-1/2 mx-6">
+        <li>Support for multiple payment gateways</li>
+        <li>
+          Multiple payment options- Debit Card, Credit Card, Netbanking, Prepaid
+          Wallets Store Credits
+        </li>
+        <li>SSL certified and secure payment gateways</li>
+        <li>Location auto­detection for currency</li>
+        <li>Offline payments &amp; Cash on Delivery</li>
+      </ul>
+      <ul class="w-1/2 mx-6">
+        <li>Support for multiple payment gateways</li>
+        <li>
+          Multiple payment options- Debit Card, Credit Card, Netbanking, Prepaid
+          Wallets Store Credits
+        </li>
+        <li>SSL certified and secure payment gateways</li>
+        <li>Location auto­detection for currency</li>
+        <li>Offline payments &amp; Cash on Delivery</li>
+      </ul>
+    </div>
   </div>
   <div class="lg:order-last order-first w-full lg:w-1/2">
     <img src="img/lady-left.png" class="w-full" alt="" />
@@ -26,24 +40,34 @@
 </div>
 <!-- 2nd section -->
 <!-- 3rd section -->
-<div class="flex flex-wrap bg-gray-100 px-2 lg:px-64 lg:mb-12">
+<div class="flex flex-wrap px-2 lg:px-64 lg:mb-12">
   <div class="leading-loose w-full lg:w-1/2 pr-0 lg:pr-12 mb-8">
     <img src="img/mobile-hand.png" class="w-full" alt="" />
   </div>
   <div class="w-full lg:w-1/2">
     <h1 class="text-4xl mb-2">What is Mobile Commerce?</h1>
-    <p class=" flex align-left mb-3">
-      Mobile commerce, or mCommerce, refers to all types of transactions
-      performed on mobile devices, including mobile banking, money transfers,
-      and of course shopping! Since mobile devices are so popular for browsing
-      the internet, it's only natural that users love to do their shopping on
-      mobile, too.
-    </p>
-    However, eCommerce and mobile commerce are not exactly the same. While
-    mCommerce is similar to eCommerce, it requires a different approach that
-    online store owners need to follow to ensure that their eCommerce websites
-    are fully optimized for the mobile experience. As an eCommerce business
-    owner, understanding of mobile optimization is essential to your success.
+    <div class="flex">
+      <ul class="w-1/2 mx-6">
+        <li>Support for multiple payment gateways</li>
+        <li>
+          Multiple payment options- Debit Card, Credit Card, Netbanking, Prepaid
+          Wallets Store Credits
+        </li>
+        <li>SSL certified and secure payment gateways</li>
+        <li>Location auto­detection for currency</li>
+        <li>Offline payments &amp; Cash on Delivery</li>
+      </ul>
+      <ul class="w-1/2 mx-6">
+        <li>Support for multiple payment gateways</li>
+        <li>
+          Multiple payment options- Debit Card, Credit Card, Netbanking, Prepaid
+          Wallets Store Credits
+        </li>
+        <li>SSL certified and secure payment gateways</li>
+        <li>Location auto­detection for currency</li>
+        <li>Offline payments &amp; Cash on Delivery</li>
+      </ul>
+    </div>
   </div>
 </div>
 
--- src/components/features/SmallContent.svelte
+++ src/components/features/SmallContent.svelte
@@ -1,3 +1,62 @@
+<script>
+  let features = [
+    {
+      key: "Powerfull search",
+      val:
+        "Leveraging the power of elastic search technology, we achieve the exact match of the searched product, giving user the ease to use and enlightment."
+    },
+    {
+      key: "Facets",
+      val:
+        "Facets helps to filter the search results further in the product listing/search/category page to narrow down the results. Facets can be based upon price range, brand, size, category, colors, key features."
+    },
+    {
+      key: "Product details",
+      val:
+        "The Customers can click on a particular product and navigate to the product page to view complete display of the product, along with descriptions, offers and discounts that are applied to that product, and so on."
+    },
+    {
+      key: "Wishlist",
+      val:
+        "Customers have option to add a product to Wishlist for later purchase"
+    },
+    { key: "SEO", val: "100% Search Engine Friendly, Google SiteMap support." },
+    {
+      key: "Marketing & Promotions",
+      val: "Coupons, discounts and different promotion options."
+    },
+    {
+      key: "Catalogue Browsing",
+      val:
+        "Multiple images for products, options for extensive reviews, wishlists and much more."
+    },
+    {
+      key: "Catalog Management",
+      val:
+        "Inventory management, batch import and export of products, additional product attributes."
+    },
+    {
+      key: "Customer Accounts",
+      val: "Order status and history, e-mail for products in the wishlist."
+    },
+    {
+      key: "Analytics & Reporting",
+      val:
+        "The script is integrated with Google Analytics and offers many different reports."
+    },
+    {
+      key: "Customer Service",
+      val:
+        "Enhanced features for customers' accounts, Contact Us form, comprehensive order tracking and history, customizable order e-mails."
+    },
+    {
+      key: "Order Management",
+      val:
+        "Create orders through admin area, create multiple invoices shipments and credit memos, call center order creation option."
+    }
+  ];
+</script>
+
 <style>
   /* .c1 {
     height: 200px;
@@ -13,32 +72,20 @@
     Get 100+ features out of the box with Next Cart's integrated per-transaction
     pricing
   </h2>
-  <div class="flex justify-between">
-    <div
-      class="w-1/2 flex flex-col md:flex-row border rounded-lg px-6 py-8
-      shadow-2xl m-2 text-center md:text-left">
-      <i class="fa fa-clock text-indigo-500 text-6xl md:mr-8" />
-      <!-- <img src="img/rocket.svg" class="flex " alt="" /> -->
-      <div class="flex flex-col">
-        <h1 class="mb-2 text-xl">Getting Started</h1>
-        <div class="text-ms">
-          Get up to speed. Follow our Quick Start Guide to begin building
-          flexible, scalable technical solutions for businesses.
+  <div class="flex justify-between flex-wrap">
+    {#each features as f}
+      <div class="w-1/2 lg:w-1/4 p-2">
+        <div
+          class="flex flex-col md:flex-row border rounded-lg px-6 py-8
+          shadow-2xl text-center md:text-left">
+          <i class="fa fa-clock text-indigo-500 text-6xl md:mr-8" />
+          <!-- <img src="img/rocket.svg" class="flex " alt="" /> -->
+          <div class="flex flex-col">
+            <h1 class="mb-2 text-xl">{f.key}</h1>
+            <div class="text-ms">{f.val}</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div
-      class="w-1/2 flex flex-col md:flex-row border rounded-lg px-6 py-8
-      shadow-2xl m-2 text-center md:text-left">
-      <i class="fa fa-clock text-indigo-500 text-6xl md:mr-8" />
-      <!-- <img src="img/rocket.svg" class="flex " alt="" /> -->
-      <div class="flex flex-col">
-        <h1 class="mb-2 text-xl">Getting Started</h1>
-        <div class="text-ms">
-          Get up to speed. Follow our Quick Start Guide to begin building
-          flexible, scalable technical solutions for businesses.
-        </div>
-      </div>
-    </div>
+    {/each}
   </div>
 </div>

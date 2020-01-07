076b684f935a5f1cc58b87b1cfe1b5433bf4c560
Swadesh Behera <itswadesh@gmail.com>
2020-01-07 14:02:07 +0530

    pricing design

--- src/routes/pricing.svelte
+++ src/routes/pricing.svelte
@@ -16,13 +16,17 @@
   .gs-3 {
     background-color: rgb(5, 213, 255);
   }
-  .c1 {
-    border-top-width: 16px;
-    border-color: rgb(45, 182, 45);
+  .b-black {
+    border-top-width: 32px;
+    border-color: #111;
   }
-  .c2 {
-    border-top-width: 16px;
-    border-color: rgb(106, 42, 196);
+  .b-yellow {
+    border-top-width: 32px;
+    border-color: #f6e05e;
+  }
+  .b-orange {
+    border-top-width: 32px;
+    border-color: #ff7426;
   }
 </style>
 
@@ -31,13 +35,13 @@
 </svelte:head>
 
 <Header />
-<div class="teacher flex flex-wrap justify-center">
+<div class="flex flex-wrap justify-center">
   <!-- student 1 -->
   <div class="w-full lg:w-1/3 p-2 lg:pr-8 lg:pl-8">
     <div
-      class="c1 flex flex-col mb-3 lg:my-12 justify-between text-center border-0
-      rounded-lg shadow-lg bg-white p-10 lg:p-8">
-      <h1 class="text-3xl">Free</h1>
+      class="b-black flex flex-col mb-3 lg:my-12 justify-between text-center
+      border-0 rounded-lg shadow-lg bg-white p-10 lg:p-8">
+      <h1 class="text-4xl mb-2 font-bold">Free</h1>
       <h2 class="text-green-500 text-xl mb-6">For developers</h2>
       <div class="text-center">
         <ul class="text-gray-600">
@@ -54,7 +58,7 @@
       </div>
       <a
         href="https://github.com/itswadesh/sapper-ecommerce"
-        class="gs-1 text-white p-2 rounded shadow-lg ">
+        class="gs-1 text-white p-2 rounded-full shadow-lg ">
         Download
       </a>
     </div>
@@ -63,9 +67,9 @@
   <!-- student 2 -->
   <div class="w-full lg:w-1/3 p-2 lg:pr-8">
     <div
-      class="c2 flex flex-col mb-3 lg:my-12 justify-between text-center border-0
-      rounded-lg shadow-lg bg-white p-8">
-      <h1 class="text-3xl">
+      class="b-orange flex flex-col mb-3 lg:my-12 justify-between text-center
+      border-0 rounded-lg shadow-lg bg-white p-8">
+      <h1 class="text-4xl mb-2 font-bold">
         Premium
         <!-- <span class="text-sm text-mute">(For Store Owners)</span> -->
       </h1>
@@ -101,13 +105,13 @@
   <!-- student 3 -->
   <div class="w-full lg:w-1/3 p-2 lg:pr-8">
     <div
-      class="c2 flex flex-col mb-3 lg:my-12 justify-between text-center border-0
-      rounded-lg shadow-lg bg-white p-8">
-      <h1 class="text-3xl">Enterprise</h1>
+      class="b-yellow flex flex-col mb-3 lg:my-12 justify-between text-center
+      border-0 rounded-lg shadow-lg bg-white p-8">
+      <h1 class="text-4xl mb-2 font-bold">Enterprise</h1>
       <h2 class="text-blue-500 text-xl mb-6">$24999.00</h2>
       <div class="text-center">
         <ul class="text-gray-600">
-          <li class="mb-4 text-red-500">All features of Premium</li>
+          <li class="mb-4 text-red-500">All features of Premium +</li>
           <li class="mb-4">Custom Theme Integration</li>
           <li class="mb-4">Discount Coupons</li>
           <li class="mb-4">Promotions</li>

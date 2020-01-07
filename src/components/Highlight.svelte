8c46f25fe0ebc36a486632c9d5b461fa65eb70e7
Ashutosh <ashutoshpanda3714@gmail.com>
2020-01-07 11:29:26 +0530

    imagesforhighlightsection

--- src/components/Highlight.svelte
+++ src/components/Highlight.svelte
@@ -3,7 +3,7 @@
     <div
       class="flex flex-col justify-center text-center items-center p-8 w-full
       lg:w-1/3">
-      <img src="/img/proven-talent.svg" alt="" class="h-24 m-8" />
+      <img src="img/fastestexperience.svg" alt="" class="h-24 m-8" />
       <div class="m-4">
         <h1 class="text-3xl mb-2 font-bold">Faster Experience</h1>
         Give your customers a seemless browsing experience.
@@ -12,7 +12,7 @@
     <div
       class=" flex flex-col justify-center text-center items-center p-8 w-full
       lg:w-1/3">
-      <img src="img/b img 2.svg" alt="" class="h-24 m-8" />
+      <img src="img/flexiblescope.svg" alt="" class="h-24 m-8" />
       <div class="m-4 ">
         <h1 class="text-3xl mb-2 font-bold">Flexible scope</h1>
         Customize it with your unique idea to make a impact.
@@ -21,7 +21,7 @@
     <div
       class=" flex flex-col justify-center text-center items-center p-8 w-full
       lg:w-1/3">
-      <img src="img/b-img-3.svg" alt="" class="h-24 m-8" />
+      <img src="img/building.svg" alt="" class="h-24 m-8" />
       <div class="m-4">
         <h1 class="text-3xl mb-2 font-bold">Enterprise standard</h1>
         All ground work has been completed with enterprise level ecommerce

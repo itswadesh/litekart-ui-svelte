2a4c1bba7b82efe3d7b0941e9ca87d37d15ccccf
Ashutosh <ashutoshpanda3714@gmail.com>
2020-01-08 14:50:20 +0530

    IMAGECHANGE

--- src/routes/details.svelte
+++ src/routes/details.svelte
@@ -5,27 +5,43 @@
 </script>
 
 <Header />
-<div class="border border-gray-400 bg-gray-300 p-10">
-  <h1 class="text-2xl">Technologies Used</h1>
-  <p>Serverless</p>
-  <p>Docker</p>
-  <p>Elastic Search</p>
-  <p>MongoDB</p>
-  <p>S3</p>
-  <p>NodeJS</p>
-  <p>VueJS</p>
-  <p>Svelte</p>
-  <p>Express</p>
+<div class=" border-gray-400 bg-gray-300 p-10">
+  <h1 class="text-2xl flex justify-center ">Technologies Used</h1>
+  <div
+    class="border flex text-center justify-around border-gray-400 bg-gray-300
+    p-10">
+    <img src="img/techused.png" />
+  </div>
+  <div class="flex">
+    <ul>
+      <li>nodejs</li>
+      <li>docker</li>
+      <li>svelte</li>
+    </ul>
+  </div>
 </div>
 
-<div class="border border-gray-400 bg-gray-300 p-10">
-  <h1 class="text-2xl">Hosting Platforms</h1>
-  <p>Digital Ocean</p>
-  <p>Amazon Cloud</p>
-  <p>Google Cloud</p>
-  <p>Vultr</p>
+<div class="border border-gray-400 bg-gray-300 p-10 justify-around">
+  <h1 class="text-3xl text-center mb-6">Hosting Platforms</h1>
+  <div class="flex flex-col ">
+    <div class="flex text-center justify-center p-4 ">
+      <p class="p-4">
+        <img src="img/digitalocean.png" />
+      </p>
+      <p class="p-4">
+        <img src="img/amazoncloud.png" />
+      </p>
+    </div>
+    <div class=" flex text-center justify-center">
+      <p class="p-4">
+        <img src="img/googlecloud.png" />
+      </p>
+      <p class="p-4">
+        <img src="img/vurtle.png" />
+      </p>
+    </div>
+  </div>
 </div>
-
 <SmallContent />
 <Footer />
 

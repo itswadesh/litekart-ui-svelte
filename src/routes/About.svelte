51e06c3111f58198aa11f6ae848a2a8e6433446d
Swadesh Behera <itswadesh@gmail.com>
2020-01-11 11:47:42 +0530

    About, Privacy Policy

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -24,20 +24,18 @@
     <p
       class=" flex justify-center mb-4 text-center text-xl font-semibold
       leading-normal">
-      Shiprocket 360, formerly Kartrocket, is a product of Delhi-based BigFoot
-      Retail Solution. It offers an end-to-end eCommerce solution to Small and
-      Medium-Sized Enterprise businesses to strengthen their footprints in the
-      eCommerce space.
+      Litekart, formerly Arialshop, is a product of Odisha, India based
+      Javascript e-commerce Solution. It offers an fullstack eCommerce website
+      to Enterprise businesses to strengthen their footprints in the eCommerce
+      space.
     </p>
     <p class="flex justify-center text-center text-ms mb-6 font-medium">
-      Founded in 2012, Shiprocket 360 has acquired 5,000 happy clients to date.
-      If you’re an eCommerce business owner, all you need to do is choose your
-      goal and we will help you develop the optimal technology platform for your
-      business.Our range of eCommerce solutions includes omnichannel retail
-      solutions, multivendor website development, hyperlocal marketplace
-      solutions and many more.We are continuously evolving to drive eCommerce
-      for Indian merchants so that they can enable a better experience for their
-      customers using our technology and analytics at every stage of the cycle.
+      Founded in 2010, Litekart has acquired 5,000 happy clients to date. If
+      you’re an eCommerce business owner, all you need to do is choose your goal
+      and we will help you develop the optimal technology platform for your
+      business. We are continuously evolving to drive eCommerce for global
+      merchants so that they can enable a better experience for their customers
+      using our technology and analytics at every stage of the cycle.
     </p>
   </div>
 </div>
@@ -46,39 +44,37 @@
   <div class="flex-1 flex flex-col bg-white text-center shadow m-6 rounded-lg">
     <img src="img/mission.png" alt=" " class=" " />
     <p class="p-4">
-      With a mission of “Selling Online Simplified,” Shiprocket 360 aims to
-      offer an easy ecommerce enablement platform where small retailers and
-      SME’s can start their online store with their brand name, at the best
-      prices.
+      With a mission of “Amazing customer experience” Litekart aims to offer an
+      easy ecommerce enablement platform where small retailers and SME’s can
+      start their online store with their brand name, at the best prices.
     </p>
   </div>
   <div class="flex-1 flex flex-col bg-white text-center shadow m-6 rounded ">
     <img src="img/vison.png" alt="" class=" " />
     <p class="p-4">
-      To simplify e-tailing for Indian merchants by offering the complete
-      ecommerce solution, so that they can easily come up with their online
-      store and start selling online.
+      To simplify administration, cart checkout and payments for merchants by
+      offering the complete ecommerce solution, so that they can easily come up
+      with their online store and start selling online.
     </p>
   </div>
   <div class="flex-1 flex flex-col bg-white text-center shadow m-6 rounded ">
     <img src="img/values.png" alt="" class=" " />
     <p class="p-4">
-      Shiprocket 360 wants to reduce ecommerce to its bare bones for small
-      retailers and SME’s. We bridge the gap between technology and innovation
-      by providing an easy yet powerful ecommerce enablement platform,
-      Shiprocket 360.
+      Litekart wants to reduce ecommerce to its bare bones for small retailers.
+      We bridge the gap between technology and innovation by providing an easy
+      yet powerful ecommerce enablement platform, Litekart.
     </p>
   </div>
 </div>
 
 <div class="py-12 justify-center mx-5">
   <h3 class="text-center text-4xl font-medium ">
-    Experience Work As A Rocketeer!
+    Experience Work As A Innovator!
   </h3>
   <p class=" flex justify-center">__________________</p>
   <p class="py-6 flex justify-center text-center ">
-    At Shiprocket 360, we don’t just offer jobs, but a career. If you are crazy
-    about work and party, then we are waiting for you, eagerly!
+    At Litekart, we don’t just offer jobs, but a career. If you are crazy about
+    work and party, then we are waiting for you, eagerly!
   </p>
   <div class="flex justify-center">
     <button

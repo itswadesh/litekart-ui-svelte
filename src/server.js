3df3e683558cd258da0f5edd414071c649914aac
itswadesh <itswadesh@gmail.com>
2020-01-13 22:19:20 +0530

    fix: now

--- src/server.js
+++ src/server.js
@@ -2,21 +2,23 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-import { apiUrl } from './config'
 
-const { PORT, NODE_ENV, API_URL } = process.env;
+console.log('imports done')
 
-const proxy = require('http-proxy-middleware');
-const apiProxy = proxy('/api', { target: API_URL || apiUrl, changeOrigin: true });
-
+const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
-polka()
+
+const app = polka() // You can also use Express
   .use(
     compression({ threshold: 0 }),
     sirv('static', { dev }),
-    apiProxy,
     sapper.middleware()
   )
-  .listen(PORT, err => {
-    if (err) console.log('error', err);
-  });
+
+console.log('PORT is', PORT)
+
+app.listen(PORT, err => {
+  if (err) console.log('error', err);
+})
+
+export default app.handler

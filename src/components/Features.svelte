9b5ec0f71410d1c5aee221d2612fd32a53f303b8
Swadesh Behera <itswadesh@gmail.com>
2020-01-16 15:45:47 +0530

    icons for features

--- src/components/Features.svelte
+++ src/components/Features.svelte
@@ -13,8 +13,13 @@
   .scroll div {
     min-width: 150px;
   }
-  .icon {
+  .svg-inline--fa {
+    width: 2.5rem;
+    height: 2.5rem;
     color: #ff7426;
+  }
+  svg {
+    margin-bottom: 1rem;
   }
 </style>
 
@@ -23,40 +28,205 @@
   <div class="scroll flex">
     <div
       class="flex-1 border-r border-gray-400 px-6 py-4 flex flex-col text-center
-      ">
-      <img src="svg/low-vision-solid.svg" alt="" class="h-20 w-12 " />
+      items-center ">
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="low-vision"
+        class="svg-inline--fa fa-low-vision fa-w-18"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512">
+        <path
+          fill="currentColor"
+          d="M569.344 231.631C512.96 135.949 407.81 72 288 72c-28.468 0-56.102
+          3.619-82.451 10.409L152.778
+          10.24c-7.601-10.858-22.564-13.5-33.423-5.9l-13.114 9.178c-10.86
+          7.601-13.502 22.566-5.9 33.426l43.131 58.395C89.449 131.73 40.228
+          174.683 6.682 231.581c-.01.017-.023.033-.034.05-8.765 14.875-8.964
+          33.528 0 48.739 38.5 65.332 99.742 115.862 172.859 141.349L55.316
+          244.302A272.194 272.194 0 0 1 83.61 208.39l119.4 170.58h.01l40.63
+          58.04a330.055 330.055 0 0 0 78.94 1.17l-189.98-271.4a277.628 277.628 0
+          0 1 38.777-21.563l251.836 356.544c7.601 10.858 22.564 13.499 33.423
+          5.9l13.114-9.178c10.86-7.601 13.502-22.567
+          5.9-33.426l-43.12-58.377-.007-.009c57.161-27.978 104.835-72.04
+          136.81-126.301a47.938 47.938 0 0 0 .001-48.739zM390.026
+          345.94l-19.066-27.23c24.682-32.567 27.711-76.353 8.8-111.68v.03c0
+          23.65-19.17 42.82-42.82 42.82-23.828 0-42.82-19.349-42.82-42.82
+          0-23.65 19.17-42.82
+          42.82-42.82h.03c-24.75-13.249-53.522-15.643-79.51-7.68l-19.068-27.237C253.758
+          123.306 270.488 120 288 120c75.162 0 136 60.826 136 136 0
+          34.504-12.833 65.975-33.974 89.94z" />
+      </svg>
       Cutting-edge Technologies
     </div>
     <div
       class="flex-1 border-r border-gray-400 px-6 py-4 flex flex-col text-center
-      ">
-      <img src="svg/tv-solid.svg" alt="" class="h-20 w-12 " />
+      items-center ">
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="tv"
+        class="svg-inline--fa fa-tv fa-w-20"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 640 512">
+        <path
+          fill="currentColor"
+          d="M592 0H48A48 48 0 0 0 0 48v320a48 48 0 0 0 48 48h240v32H112a16 16 0
+          0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0
+          0-16-16H352v-32h240a48 48 0 0 0 48-48V48a48 48 0 0 0-48-48zm-16
+          352H64V64h512z" />
+      </svg>
       NodeJS + MongoDB + ES + Microservices
     </div>
     <div
       class="flex-1 border-r border-gray-400 px-6 py-4 f flex flex-col
-      text-center">
-      <img src="svg/tools-solid.svg" alt="" class="h-20 w-12 " />
+      text-center items-center">
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="tools"
+        class="svg-inline--fa fa-tools fa-w-16"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512">
+        <path
+          fill="currentColor"
+          d="M501.1 395.7L384 278.6c-23.1-23.1-57.6-27.6-85.4-13.9L192
+          158.1V96L64 0 0 64l96 128h62.1l106.6 106.6c-13.6 27.8-9.2 62.3 13.9
+          85.4l117.1 117.1c14.6 14.6 38.2 14.6 52.7 0l52.7-52.7c14.5-14.6
+          14.5-38.2 0-52.7zM331.7 225c28.3 0 54.9 11 74.9 31l19.4 19.4c15.8-6.9
+          30.8-16.5 43.8-29.5 37.1-37.1 49.7-89.3
+          37.9-136.7-2.2-9-13.5-12.1-20.1-5.5l-74.4 74.4-67.9-11.3L334
+          98.9l74.4-74.4c6.6-6.6 3.4-17.9-5.7-20.2-47.4-11.7-99.6.9-136.6
+          37.9-28.5 28.5-41.9 66.1-41.2 103.6l82.1 82.1c8.1-1.9 16.5-2.9
+          24.7-2.9zm-103.9 82l-56.7-56.7L18.7 402.8c-25 25-25 65.5 0 90.5s65.5
+          25 90.5 0l123.6-123.6c-7.6-19.9-9.9-41.6-5-62.7zM64 472c-13.2
+          0-24-10.8-24-24 0-13.3 10.7-24 24-24s24 10.7 24 24c0 13.2-10.7 24-24
+          24z" />
+      </svg>
       Customize it the way you want
     </div>
     <div
-      class="flex-1 border-r border-gray-400 px-6 py-4 flex flex-col text-center">
-      <img src="svg/sign-in-alt-solid.svg" alt="" class="h-20 w-12 " />
+      class="flex-1 border-r border-gray-400 px-6 py-4 flex flex-col text-center
+      items-center">
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="sign-in-alt"
+        class="svg-inline--fa fa-sign-in-alt fa-w-16"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512">
+        <path
+          fill="currentColor"
+          d="M416 448h-84c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h84c17.7 0
+          32-14.3 32-32V160c0-17.7-14.3-32-32-32h-84c-6.6
+          0-12-5.4-12-12V76c0-6.6 5.4-12 12-12h84c53 0 96 43 96 96v192c0 53-43
+          96-96 96zm-47-201L201 79c-15-15-41-4.5-41 17v96H24c-13.3 0-24 10.7-24
+          24v96c0 13.3 10.7 24 24 24h136v96c0 21.5 26 32 41 17l168-168c9.3-9.4
+          9.3-24.6 0-34z" />
+      </svg>
       Progressive ecommerce app made with NodeJS
     </div>
     <div
-      class="flex-1 border-r border-gray-400 px-6 py-4 flex flex-col text-center">
-      <img src="svg/search-dollar-solid.svg" alt="" class="h-20 w-12 " />
+      class="flex-1 border-r border-gray-400 px-6 py-4 flex flex-col text-center
+      items-center">
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="search-dollar"
+        class="svg-inline--fa fa-search-dollar fa-w-16"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512">
+        <path
+          fill="currentColor"
+          d="M505.04 442.66l-99.71-99.69c-4.5-4.5-10.6-7-17-7h-16.3c27.6-35.3
+          44-79.69 44-127.99C416.03 93.09 322.92 0 208.02 0S0 93.09 0
+          207.98s93.11 207.98 208.02 207.98c48.3 0 92.71-16.4 128.01-44v16.3c0
+          6.4 2.5 12.5 7 17l99.71 99.69c9.4 9.4 24.6 9.4 33.9
+          0l28.3-28.3c9.4-9.4 9.4-24.59.1-33.99zm-297.02-90.7c-79.54
+          0-144-64.34-144-143.98 0-79.53 64.35-143.98 144-143.98 79.54 0 144
+          64.34 144 143.98 0 79.53-64.35 143.98-144
+          143.98zm27.11-152.54l-45.01-13.5c-5.16-1.55-8.77-6.78-8.77-12.73
+          0-7.27 5.3-13.19 11.8-13.19h28.11c4.56 0 8.96 1.29 12.82 3.72 3.24
+          2.03 7.36 1.91 10.13-.73l11.75-11.21c3.53-3.37
+          3.33-9.21-.57-12.14-9.1-6.83-20.08-10.77-31.37-11.35V112c0-4.42-3.58-8-8-8h-16c-4.42
+          0-8 3.58-8 8v16.12c-23.63.63-42.68 20.55-42.68 45.07 0 19.97 12.99
+          37.81 31.58 43.39l45.01 13.5c5.16 1.55 8.77 6.78 8.77 12.73 0 7.27-5.3
+          13.19-11.8 13.19h-28.1c-4.56
+          0-8.96-1.29-12.82-3.72-3.24-2.03-7.36-1.91-10.13.73l-11.75 11.21c-3.53
+          3.37-3.33 9.21.57 12.14 9.1 6.83 20.08 10.77 31.37 11.35V304c0 4.42
+          3.58 8 8 8h16c4.42 0 8-3.58 8-8v-16.12c23.63-.63 42.68-20.54
+          42.68-45.07 0-19.97-12.99-37.81-31.59-43.39z" />
+      </svg>
       High on-page SEO + off-page ready
     </div>
     <div
-      class="flex-1 border-r border-gray-400 px-6 py-4 flex flex-col text-center">
-      <img src="svg/chart-bar-solid.svg" alt="" class="h-20 w-12 " />
+      class="flex-1 border-r border-gray-400 px-6 py-4 flex flex-col text-center
+      items-center">
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="chart-bar"
+        class="svg-inline--fa fa-chart-bar fa-w-16"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512">
+        <path
+          fill="currentColor"
+          d="M332.8 320h38.4c6.4 0 12.8-6.4
+          12.8-12.8V172.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8
+          12.8v134.4c0 6.4 6.4 12.8 12.8 12.8zm96 0h38.4c6.4 0 12.8-6.4
+          12.8-12.8V76.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8
+          12.8v230.4c0 6.4 6.4 12.8 12.8 12.8zm-288 0h38.4c6.4 0 12.8-6.4
+          12.8-12.8v-70.4c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8
+          12.8v70.4c0 6.4 6.4 12.8 12.8 12.8zm96 0h38.4c6.4 0 12.8-6.4
+          12.8-12.8V108.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8
+          12.8v198.4c0 6.4 6.4 12.8 12.8 12.8zM496
+          384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67
+          14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16z" />
+      </svg>
       Scalable as well as Performant
     </div>
     <div
-      class="flex-1 border-r border-gray-400 px-6 py-4 flex flex-col text-center">
-      <img src="svg/universal-access-solid.svg" alt="" class="h-20 w-12 " />
+      class="flex-1 border-r border-gray-400 px-6 py-4 flex flex-col text-center
+      items-center">
+      <svg
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="universal-access"
+        class="svg-inline--fa fa-universal-access fa-w-16"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512">
+        <path
+          fill="currentColor"
+          d="M256 48c114.953 0 208 93.029 208 208 0 114.953-93.029 208-208
+          208-114.953 0-208-93.029-208-208 0-114.953 93.029-208
+          208-208m0-40C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033
+          248-248S392.967 8 256 8zm0 56C149.961 64 64 149.961 64 256s85.961 192
+          192 192 192-85.961 192-192S362.039 64 256 64zm0 44c19.882 0 36 16.118
+          36 36s-16.118 36-36 36-36-16.118-36-36 16.118-36 36-36zm117.741
+          98.023c-28.712 6.779-55.511 12.748-82.14 15.807.851 101.023 12.306
+          123.052 25.037 155.621 3.617 9.26-.957 19.698-10.217 23.315-9.261
+          3.617-19.699-.957-23.316-10.217-8.705-22.308-17.086-40.636-22.261-78.549h-9.686c-5.167
+          37.851-13.534 56.208-22.262 78.549-3.615 9.255-14.05 13.836-23.315
+          10.217-9.26-3.617-13.834-14.056-10.217-23.315 12.713-32.541
+          24.185-54.541
+          25.037-155.621-26.629-3.058-53.428-9.027-82.141-15.807-8.6-2.031-13.926-10.648-11.895-19.249s10.647-13.926
+          19.249-11.895c96.686 22.829 124.283 22.783 220.775 0 8.599-2.03 17.218
+          3.294 19.249 11.895 2.029 8.601-3.297 17.219-11.897 19.249z" />
+      </svg>
       Modern API based architecture
     </div>
     <a
